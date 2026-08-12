293286cca6ca16c4418b8a8b703782d646bea2e0
Ceyhun <rzayev-ceyhun-99@mail.ru>
2026-08-12 20:26:30 +0400

    add project

---
+++ books_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  books_app
+//
+//  Created by Ceyhun Rzayev on 12.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ books_app/books_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  books_appApp.swift
+//  books_app
+//
+//  Created by Ceyhun Rzayev on 12.08.26.
+//
+
+import SwiftUI
+
+@main
+struct books_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
